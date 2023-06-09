1fada8790e7aefd2b93a600201eafb3abe697b4e
Henrique Herbst Rosa <henriquehhr@gmail.com>
2023-06-08 21:04:30 -0300

    adicionado classe para inicializar a cena

---
+++ src/SceneInit.ts
@@ -0,0 +1,81 @@
+import * as THREE from 'three';
+import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
+import { FontLoader, Font } from 'three/examples/jsm/loaders/FontLoader.js';
+
+export default class SceneInit {
+  // NOTE: Core components to initialize Three.js app.
+  scene: THREE.Scene;
+  camera: THREE.PerspectiveCamera;
+  renderer: THREE.WebGLRenderer;
+
+  // NOTE: Camera params;
+  fov = 65;
+  nearPlane = 0.1;
+  farPlane = 1000;
+
+  // NOTE: Additional components.
+  controls: TrackballControls;
+  quaternion: THREE.Quaternion;
+
+  rotateWithCamera: THREE.Mesh[] = [];
+  raycaster: THREE.Raycaster;
+  position: THREE.Vector2;
+
+  constructor() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      this.fov,
+      window.innerWidth / window.innerHeight,
+      this.nearPlane,
+      this.farPlane
+    );
+    this.camera.position.z = 5;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    this.controls = new TrackballControls(this.camera, this.renderer.domElement);
+    this.controls.rotateSpeed = 4;
+    this.quaternion = new THREE.Quaternion();
+    this.raycaster = new THREE.Raycaster();
+    this.position = new THREE.Vector2();
+
+    window.addEventListener('resize', () => this.onWindowResize(), false);
+  }
+
+  public loadFont(fontPath: string): Promise<Font> {
+    const fontLoader = new FontLoader();
+    const font = new Promise((resolve) => {
+      fontLoader.load(fontPath, resolve);
+    });
+    return font as Promise<Font>;
+  }
+
+  public setRotationFromQuaternion(object: THREE.Mesh) {
+    this.rotateWithCamera.push(object);
+  }
+
+  public getIntersectedObjects(x: number, y: number) {
+    this.position.x = (x / window.innerWidth) * 2 - 1;
+    this.position.y = - (y / window.innerHeight) * 2 + 1;
+    this.raycaster.setFromCamera(this.position, this.camera);
+    const intersects = this.raycaster.intersectObjects(this.scene.children) as THREE.Intersection<THREE.Mesh>[];
+    return intersects;
+  }
+
+  public animate() {
+    window.requestAnimationFrame(this.animate.bind(this));
+    this.controls.update();
+    this.camera.getWorldQuaternion(this.quaternion);
+    this.rotateWithCamera.forEach(obj => obj.setRotationFromQuaternion(this.quaternion));
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  private onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import MineSweeper3D from './MineSweeper3D.js';
+import SceneInit from './SceneInit.js';
 
 import * as THREE from 'three';
-import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { Position } from './types.js';
 
-const mineSweeper = new MineSweeper3D(6, 6, 6, 8);
+const mineSweeper = new MineSweeper3D(6, 6, 6, 4);
+const sceneInit = new SceneInit();
 
 // Tamanho e quantidade de cubos menores
 const cubeSize = 0.4;
@@ -44,266 +44,218 @@ for (let i = -1; i <= 1; i++) {
   }
 }
 
-const fontLoader = new FontLoader();
-fontLoader.load('assets/helvetiker_regular.typeface.json', (font) => {
+const fontPath = 'assets/helvetiker_regular.typeface.json';
+const font = await sceneInit.loadFont(fontPath);
 
-  // Crie uma função para criar um objeto de texto
-  function createTextObject(text: string, position: THREE.Vector3, color: number) {
+// Crie uma função para criar um objeto de texto
+function createTextObject(text: string, position: THREE.Vector3, color: number) {
 
-    const textMaterial = new THREE.MeshBasicMaterial({
-      color: color,
-      side: THREE.DoubleSide
-    });
-    const shapes = font.generateShapes(text, 0.17);
-    const textGeometry = new THREE.ShapeGeometry(shapes);
+  const textMaterial = new THREE.MeshBasicMaterial({
+    color: color,
+    side: THREE.DoubleSide
+  });
+  const shapes = font.generateShapes(text, 0.17);
+  const textGeometry = new THREE.ShapeGeometry(shapes);
 
-    /*const textMaterial = new THREE.MeshBasicMaterial({ color: color });
-    const textGeometry = new TextGeometry(text, {
-      font: font,
-      size: 0.2,
-      height: 0.05,
-      curveSegments: 12,
-    });*/
-    /*textGeometry.computeBoundingBox();
-    if (textGeometry.boundingBox) {
-      const textWidth = textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x;
-      const textHeight = textGeometry.boundingBox.max.y - textGeometry.boundingBox.min.y;
-      textGeometry.translate(-0.5 * textWidth, 0, 0);
-      textGeometry.translate(0, -0.5 * textHeight, 0);
-    }*/
-    const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-    textMesh.position.copy(position);
+  /*const textMaterial = new THREE.MeshBasicMaterial({ color: color });
+  const textGeometry = new TextGeometry(text, {
+    font: font,
+    size: 0.2,
+    height: 0.05,
+    curveSegments: 12,
+  });*/
+  /*textGeometry.computeBoundingBox();
+  if (textGeometry.boundingBox) {
+    const textWidth = textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x;
+    const textHeight = textGeometry.boundingBox.max.y - textGeometry.boundingBox.min.y;
+    textGeometry.translate(-0.5 * textWidth, 0, 0);
+    textGeometry.translate(0, -0.5 * textHeight, 0);
+  }*/
+  const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+  textMesh.position.copy(position);
 
-    return textMesh;
-  }
+  return textMesh;
+}
 
-  // Crie uma cena
-  const scene = new THREE.Scene();
+// Crie um grupo para os cubos menores
+const cubeGroup = new THREE.Group();
 
-  // Crie uma câmera
-  const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.z = 5;
-  camera.updateProjectionMatrix();
+const cubeMap = new Map<string, THREE.Mesh<THREE.BoxGeometry>>;
 
-  // Crie um renderizador
-  const antialias = { antialias: true }
-  const renderer = new THREE.WebGLRenderer(antialias);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+const adjacentCubes: THREE.Mesh<THREE.BoxGeometry>[][][] = [];
+// Crie os cubos menores e adicione-os ao grupo
+for (let i = 0; i < cubeCount; i++) {
+  const line: Array<any> = [];
+  adjacentCubes.push(line);
+  for (let j = 0; j < cubeCount; j++) {
+    const column: Array<any> = [];
+    adjacentCubes[i].push(column);
+    for (let k = cubeCount - 1; k >= 0; k--) {
+      const material = new THREE.MeshBasicMaterial({ color: cubeColor, transparent: false, opacity: 0.5 });
+      const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+      const cube = new THREE.Mesh(geometry, material);
+      // Crie as arestas dos cubos menores
+      const edgeMaterial = new THREE.LineBasicMaterial({ color: edgeColor, transparent: true, opacity: 0.3 });
+      const edges = new THREE.EdgesGeometry(geometry);
+      const edgesMesh = new THREE.LineSegments(edges, edgeMaterial);
+      adjacentCubes[i][j][k] = cube;
 
-  // Adicione os controles orbitais
-  const controls = new TrackballControls(camera, renderer.domElement);
-  controls.rotateSpeed = 4;
+      const positionX = (i - cubeCount / 2) * (cubeSize + spacing) + (cubeSize + spacing) / 2;
+      const positionY = (j - cubeCount / 2) * (cubeSize + spacing) + (cubeSize + spacing) / 2;
+      const positionZ = (k - cubeCount / 2) * (cubeSize + spacing) + (cubeSize + spacing) / 2;
 
-  // Crie um grupo para os cubos menores
-  const cubeGroup = new THREE.Group();
-
-  const cubeMap = new Map<string, THREE.Mesh<THREE.BoxGeometry>>;
-
-
-  const texts: THREE.Mesh[] = [];
-  const adjacentCubes: THREE.Mesh<THREE.BoxGeometry>[][][] = [];
-  // Crie os cubos menores e adicione-os ao grupo
-  for (let i = 0; i < cubeCount; i++) {
-    const line: Array<any> = [];
-    adjacentCubes.push(line);
-    for (let j = 0; j < cubeCount; j++) {
-      const column: Array<any> = [];
-      adjacentCubes[i].push(column);
-      for (let k = cubeCount - 1; k >= 0; k--) {
-        const material = new THREE.MeshBasicMaterial({ color: cubeColor, transparent: false, opacity: 0.5 });
-        const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-        const cube = new THREE.Mesh(geometry, material);
-        // Crie as arestas dos cubos menores
-        const edgeMaterial = new THREE.LineBasicMaterial({ color: edgeColor, transparent: true, opacity: 0.3 });
-        const edges = new THREE.EdgesGeometry(geometry);
-        const edgesMesh = new THREE.LineSegments(edges, edgeMaterial);
-        adjacentCubes[i][j][k] = cube;
-
-        const positionX = (i - cubeCount / 2) * (cubeSize + spacing) + (cubeSize + spacing) / 2;
-        const positionY = (j - cubeCount / 2) * (cubeSize + spacing) + (cubeSize + spacing) / 2;
-        const positionZ = (k - cubeCount / 2) * (cubeSize + spacing) + (cubeSize + spacing) / 2;
-
-        const adjacentMines = mineSweeper.fields[i][j][k].adjacentMines;
-        const isMine = mineSweeper.fields[i][j][k].mine;
-        if (adjacentMines > 0 || isMine) {
-          let textMesh: THREE.Mesh;
-          if (!isMine) {
-            textMesh = createTextObject('' + adjacentMines, new THREE.Vector3(positionX, positionY, positionZ), numberColor[adjacentMines]);
-          }
-          else {
-            textMesh = createTextObject('X', new THREE.Vector3(positionX, positionY, positionZ), 0xFFc0cb)
-          }
-          texts.push(textMesh);
-          cubeGroup.add(textMesh);
-          edgesMesh.position.set(positionX, positionY, positionZ);
-          cubeGroup.add(edgesMesh);
+      const adjacentMines = mineSweeper.fields[i][j][k].adjacentMines;
+      const isMine = mineSweeper.fields[i][j][k].mine;
+      if (adjacentMines > 0 || isMine) {
+        let textMesh: THREE.Mesh;
+        if (!isMine) {
+          textMesh = createTextObject('' + adjacentMines, new THREE.Vector3(positionX, positionY, positionZ), numberColor[adjacentMines]);
         }
-        cube.position.set(positionX, positionY, positionZ);
-        cubeGroup.add(cube);
-        cubeMap.set(`${i}${j}${k}`, cube);
-      }
-    }
-  }
-
-  // Adicione o grupo de cubos à cena
-  scene.add(cubeGroup);
-
-  const raycaster = new THREE.Raycaster();
-  const clickMouse = new THREE.Vector2();
-  let isDragging = false;
-
-  window.addEventListener("keydown", event => {
-    if (event.ctrlKey)
-      window.addEventListener("mousemove", selectAdjacentCubes, false);
-  });
-  window.addEventListener("keyup", () => {
-    window.removeEventListener("mousemove", selectAdjacentCubes, false);
-  });
-  window.addEventListener('mousedown', () => isDragging = false);
-  window.addEventListener('mousemove', () => isDragging = true);
-  window.addEventListener('mouseup', event => {
-    if (isDragging) {
-      isDragging = false;
-      return;
-    }
-    clickMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    clickMouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-    raycaster.setFromCamera(clickMouse, camera);
-    const intersects = raycaster.intersectObjects(scene.children) as THREE.Intersection<THREE.Mesh>[];
-    const { object: cube } = intersects.find(shape => (shape.object.geometry instanceof THREE.BoxGeometry)) ?? { object: null };
-    if (!cube) return;
-    if (cube.scale.x !== 1) return;
-
-    if (event.button == 2 || event.which == 3) {
-      rightClickCube(cube);
-    } else {
-      leftClickCube(cube);
-    }
-  });
-
-  function leftClickCube(cube: any) {
-    //reduceCube(cube);
-    const p = getFieldPosition(cube.position);
-    const response = mineSweeper.clickField(p);
-    const positionsToUncover = response.fieldsToUncover;
-    if (positionsToUncover?.length == 0) return;
-    cubeGroup.remove(cube);
-    if (!positionsToUncover) return;
-    for (const position of positionsToUncover) {
-      const cubeToRemove = cubeMap.get(`${position.x}${position.y}${position.z}`)
-      if (!cubeToRemove) continue;
-      cubeGroup.remove(cubeToRemove);
-    }
-    if (response.gameOver)
-      alert(response.gameOver);
-  }
-  function rightClickCube(cube: any) {
-    const p = getFieldPosition(cube.position);
-    const status = mineSweeper.flagAField(p);
-    let color;
-    if (status == 'flagged')
-      color = flaggedCubeColor;
-    else if (status == 'covered')
-      color = cubeColor;
-    if (status != 'uncovered')
-      //@ts-ignore
-      cube.material.color.set(color);
-  }
-
-  let lastIntersectedObject: THREE.Mesh | null = null;
-  function selectAdjacentCubes(event: MouseEvent) {
-    clickMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    clickMouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-    raycaster.setFromCamera(clickMouse, camera);
-    const intersects = raycaster.intersectObjects(scene.children) as THREE.Intersection<THREE.Mesh>[];
-    for (const mesh of intersects) {
-      if (mesh.object.geometry instanceof THREE.ShapeGeometry)
-        break;
-      if (mesh.object.geometry instanceof THREE.BoxGeometry) {
-        if (lastIntersectedObject)
-          changeColorOfAdjacentCubes(getFieldPosition(lastIntersectedObject.position), cubeColor);
-        return;
-      }
-    }
-    const { object: number } = intersects.find(shape => shape.object.geometry instanceof THREE.ShapeGeometry) ?? { object: null };
-
-    if (!number) {// está com o mouse fora do número ou saiu do número
-      if (lastIntersectedObject)
-        changeColorOfAdjacentCubes(getFieldPosition(lastIntersectedObject.position), cubeColor);
-      lastIntersectedObject = null;
-      return;
-    }
-    if (lastIntersectedObject !== number) { //acabou de entrar no número
-      if (lastIntersectedObject)
-        changeColorOfAdjacentCubes(getFieldPosition(lastIntersectedObject.position), cubeColor);
-      changeColorOfAdjacentCubes(getFieldPosition(number.position), selectedCubeColor);
-      lastIntersectedObject = number;
-    } else {
-      changeColorOfAdjacentCubes(getFieldPosition(number.position), selectedCubeColor);
-    }
-  }
-
-  function changeColorOfAdjacentCubes(p: Position, color: number) {
-    for (const a of adjacentFields) {
-      if (!adjacentCubes[p.x + a.x]) continue;
-      if (!adjacentCubes[p.x + a.x][p.y + a.y]) continue;
-      if (!adjacentCubes[p.x + a.x][p.y + a.y][p.z + a.z]) continue;
-      //@ts-ignore
-      adjacentCubes[p.x + a.x][p.y + a.y][p.z + a.z].material.color.set(color);
-    }
-  }
-  //function leftAndRightClickCube(intersect: THREE.Intersection[]) { }
-  /*
-    function reduceCube(cube: THREE.Object3D) {
-      if (!cube) return;
-      const initialScale = cube.scale.clone();
-      const targetScale = new THREE.Vector3(0, 0, 0);
-      const duration = 1; // Duração da animação em segundos
-      const interval = 10; // Intervalo entre os frames em milissegundos
-  
-      let currentTime = 0;
-  
-      const timer = setInterval(function () {
-        currentTime += interval / 1000; // Converter para segundos
-  
-        if (currentTime > duration) {
-          clearInterval(timer);
-          cubeGroup.remove(cube);
-        } else {
-          const t = currentTime / duration;
-          cube.scale.lerpVectors(initialScale, targetScale, t);
+        else {
+          textMesh = createTextObject('X', new THREE.Vector3(positionX, positionY, positionZ), 0xFFc0cb)
         }
-      }, interval);
+        sceneInit.setRotationFromQuaternion(textMesh);
+        cubeGroup.add(textMesh);
+        edgesMesh.position.set(positionX, positionY, positionZ);
+        cubeGroup.add(edgesMesh);
+      }
+      cube.position.set(positionX, positionY, positionZ);
+      cubeGroup.add(cube);
+      cubeMap.set(`${i}${j}${k}`, cube);
     }
-  */
-
-  function getFieldPosition(v: THREE.Vector3): Position {
-    const s = cubeSize + spacing;
-    const x = Math.round(((cubeCount * s) / 2 - (s / 2) + v.x) / s);
-    const y = Math.round(((cubeCount * s) / 2 - (s / 2) + v.y) / s);
-    const z = Math.round(((cubeCount * s) / 2 - (s / 2) + v.z) / s);
-    return { x, y, z };
   }
+}
 
-  // Função de renderização
-  function animate() {
-    requestAnimationFrame(animate);
+// Adicione o grupo de cubos à cena
+sceneInit.scene.add(cubeGroup);
 
-    // Atualize os controles
-    controls.update();
+const clickMouse = new THREE.Vector2();
+let isDragging = false;
 
-    // Mantenha o texto sempre em pé e de frente para a câmera
-    const quaternion = new THREE.Quaternion();
-    camera.getWorldQuaternion(quaternion);
-    texts.forEach(text => text.setRotationFromQuaternion(quaternion));
-
-    // Renderize a cena com a câmera
-    renderer.render(scene, camera);
+window.addEventListener("keydown", event => {
+  if (event.ctrlKey)
+    window.addEventListener("mousemove", selectAdjacentCubes, false);
+});
+window.addEventListener("keyup", () => {
+  window.removeEventListener("mousemove", selectAdjacentCubes, false);
+});
+window.addEventListener('mousedown', () => isDragging = false);
+window.addEventListener('mousemove', () => isDragging = true);
+window.addEventListener('mouseup', event => {
+  if (isDragging) {
+    isDragging = false;
+    return;
   }
+  const intersects = sceneInit.getIntersectedObjects(event.clientX, event.clientY);
+  const { object: cube } = intersects.find(shape => (shape.object.geometry instanceof THREE.BoxGeometry)) ?? { object: null };
+  if (!cube) return;
+  if (cube.scale.x !== 1) return;
 
-  // Inicie a animação
-  animate();
+  if (event.button == 2 || event.which == 3) {
+    rightClickCube(cube);
+  } else {
+    leftClickCube(cube);
+  }
 });
 
+function leftClickCube(cube: any) {
+  //reduceCube(cube);
+  const p = getFieldPosition(cube.position);
+  const response = mineSweeper.clickField(p);
+  const positionsToUncover = response.fieldsToUncover;
+  if (positionsToUncover?.length == 0) return;
+  cubeGroup.remove(cube);
+  if (!positionsToUncover) return;
+  for (const position of positionsToUncover) {
+    const cubeToRemove = cubeMap.get(`${position.x}${position.y}${position.z}`)
+    if (!cubeToRemove) continue;
+    cubeGroup.remove(cubeToRemove);
+  }
+  if (response.gameOver)
+    alert(response.gameOver);
+}
+function rightClickCube(cube: any) {
+  const p = getFieldPosition(cube.position);
+  const status = mineSweeper.flagAField(p);
+  let color;
+  if (status == 'flagged')
+    color = flaggedCubeColor;
+  else if (status == 'covered')
+    color = cubeColor;
+  if (status != 'uncovered')
+    //@ts-ignore
+    cube.material.color.set(color);
+}
 
+let lastIntersectedObject: THREE.Mesh | null = null;
+function selectAdjacentCubes(event: MouseEvent) {
+  const intersects = sceneInit.getIntersectedObjects(event.clientX, event.clientY);
+  for (const mesh of intersects) {
+    if (mesh.object.geometry instanceof THREE.ShapeGeometry)
+      break;
+    if (mesh.object.geometry instanceof THREE.BoxGeometry) {
+      if (lastIntersectedObject)
+        changeColorOfAdjacentCubes(getFieldPosition(lastIntersectedObject.position), cubeColor);
+      return;
+    }
+  }
+  const { object: number } = intersects.find(shape => shape.object.geometry instanceof THREE.ShapeGeometry) ?? { object: null };
+
+  if (!number) {// está com o mouse fora do número ou saiu do número
+    if (lastIntersectedObject)
+      changeColorOfAdjacentCubes(getFieldPosition(lastIntersectedObject.position), cubeColor);
+    lastIntersectedObject = null;
+    return;
+  }
+  if (lastIntersectedObject !== number) { //acabou de entrar no número
+    if (lastIntersectedObject)
+      changeColorOfAdjacentCubes(getFieldPosition(lastIntersectedObject.position), cubeColor);
+    changeColorOfAdjacentCubes(getFieldPosition(number.position), selectedCubeColor);
+    lastIntersectedObject = number;
+  } else {
+    changeColorOfAdjacentCubes(getFieldPosition(number.position), selectedCubeColor);
+  }
+}
+
+function changeColorOfAdjacentCubes(p: Position, color: number) {
+  for (const a of adjacentFields) {
+    if (!adjacentCubes[p.x + a.x]) continue;
+    if (!adjacentCubes[p.x + a.x][p.y + a.y]) continue;
+    if (!adjacentCubes[p.x + a.x][p.y + a.y][p.z + a.z]) continue;
+    //@ts-ignore
+    adjacentCubes[p.x + a.x][p.y + a.y][p.z + a.z].material.color.set(color);
+  }
+}
+//function leftAndRightClickCube(intersect: THREE.Intersection[]) { }
+/*
+  function reduceCube(cube: THREE.Object3D) {
+    if (!cube) return;
+    const initialScale = cube.scale.clone();
+    const targetScale = new THREE.Vector3(0, 0, 0);
+    const duration = 1; // Duração da animação em segundos
+    const interval = 10; // Intervalo entre os frames em milissegundos
+
+    let currentTime = 0;
+
+    const timer = setInterval(function () {
+      currentTime += interval / 1000; // Converter para segundos
+
+      if (currentTime > duration) {
+        clearInterval(timer);
+        cubeGroup.remove(cube);
+      } else {
+        const t = currentTime / duration;
+        cube.scale.lerpVectors(initialScale, targetScale, t);
+      }
+    }, interval);
+  }
+*/
+
+function getFieldPosition(v: THREE.Vector3): Position {
+  const s = cubeSize + spacing;
+  const x = Math.round(((cubeCount * s) / 2 - (s / 2) + v.x) / s);
+  const y = Math.round(((cubeCount * s) / 2 - (s / 2) + v.y) / s);
+  const z = Math.round(((cubeCount * s) / 2 - (s / 2) + v.z) / s);
+  return { x, y, z };
+}
+sceneInit.animate();
