3579377662a0592df8827f7c32d371b95331bea9
Henrique Herbst Rosa <henriquehhr@gmail.com>
2023-06-09 00:15:33 -0300

    adicionado configuração do vite

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    target: 'es2022',
+    polyfillDynamicImport: false,
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'src/main.js',
+    },
+  },
+});
