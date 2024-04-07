964184f3eda05d57b740d5c059e206142ac33778
Henrique H Rosa <henriquehhr@gmail.com>
2024-04-07 12:39:03 -0300

    atualizado dependência em SceneInit

--- src/SceneInit.ts
+++ src/SceneInit.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import * as TWEEN from '@tweenjs/tween.js';
-import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
-import { FontLoader, Font } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
+import { FontLoader, Font } from 'three/addons/loaders/FontLoader.js';
 import WebGPURenderer from 'three/addons/renderers/webgpu/WebGPURenderer.js';
 import { renderer } from './store.js';
 
